87a7d76de975995ec75927a9a0a8c703e3684111
Sergei Volkau <svolkau@MLLA9846.magicleap.ds>
2023-07-08 18:29:34 -0700

    23

---
+++ terraform/2-image-updater.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "updater" {
+  name             = "updater"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-image-updater"
+  namespace        = "argocd"
+  create_namespace = "true"
+  version          = "0.9.1"
+  values = [
+    "${file("updater-value.yaml")}"
+  ]
+}
